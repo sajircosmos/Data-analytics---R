3330634743d3f2ce7d2395117f1c0eb9ead79b25
sajircosmos <64122810+sajircosmos@users.noreply.github.com>
2020-04-24 23:16:00 +0530

    clustering

---
+++ Hclustering_University.R
@@ -0,0 +1,34 @@
+
+input <- read.csv("Universities_Clustering.csv")
+View(input)
+mydata <- input[,(2:7)]
+# Load data as mydata
+View(mydata)
+
+normalized_data <- scale(mydata)#excluding the university name columnbefore normalizing
+View(normalized_data)
+d <- dist(normalized_data, method = "euclidean") # distance matrix
+fit <- hclust(d, method="complete") 
+fit
+?hclust
+plot(fit) # display dendrogram
+plot(fit, hang=-1)
+groups <- cutree(fit, k=3)# cut tree into 3 clusters
+
+?cutree
+rect.hclust(fit, k=3, border="red")
+?rect.hclust
+
+membership<-as.matrix(groups)
+
+final <- data.frame(input, membership)
+View(final)
+
+final1 <- final[,c(ncol(final),1:(ncol(final)-1))]
+View(final1)
+
+?write.xlsx
+
+write.csv(final1, file="final.csv")
+
+getwd()
